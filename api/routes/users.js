65b194a1bb508aa9d45e4a6fcee347fbb6b210dc
draught <classydraught@gmail.com>
2020-04-07 22:25:18 +0530

    JWT

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const User = require("../models/user");
 const bcrypyt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 router.post("/signup", (req, res, next) => {
   User.find({ email: req.body.email })
     .exec()
@@ -39,6 +40,51 @@ router.post("/signup", (req, res, next) => {
           }
         });
       }
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      }
+      bcrypyt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed",
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({
+            message: "Auth succeded",
+            token: token,
+          });
+        } else {
+          return res.status(401).json({
+            message: "Auth failed",
+          });
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      req.status(500).json({
+        error: err,
+      });
     });
 });
 
